d207051efbb6e35eb039aedea0fabc6d468c883c
Florencia zarate <Florenciazarate2110@gmail.com>
2026-04-07 09:58:03 -0300

    fix

--- pryZarateSP1.2/frmPrincipal.cs
+++ pryZarateSP1.2/frmPrincipal.cs
@@ -13,15 +13,12 @@ namespace pryZarateSP1._2
         public frmPrincipal()
         {
             InitializeComponent();
-
-            // Suscribir eventos para mantener sincronía entre pestañas
             ucEspecialidades1.EspecialidadAdded += (s, e) => ucMedicos1.CargarEspecialidades();
             tabControl1.SelectedIndexChanged += TabControl1_SelectedIndexChanged;
         }
 
         private void TabControl1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            // cuando se selecciona la pestaña de médicos refrescar lista de especialidades
             if (tabControl1.SelectedTab == tabPageMedicos)
             {
                 ucMedicos1.CargarEspecialidades();
@@ -30,16 +27,12 @@ namespace pryZarateSP1._2
 
         private void button1_Click(object sender, EventArgs e)
         {
-            // Obtener la lista de médicos de la capa de datos
             List<Medico> medicos = Repository.GetMedicos().ToList();
-
-            // Asignar la lista como fuente de datos del DataGridView
             dataGridView1.DataSource = medicos.Select(m => new { Matricula = m.Matricula, Nombre = m.Nombre, EspecialidadId = m.EspecialidadId }).ToList();
         }
 
         private void frmPrincipal_Load(object sender, EventArgs e)
         {
-            // Cargar las especialidades al iniciar el formulario
             CargarEspecialidades();
         }
 
@@ -48,5 +41,7 @@ namespace pryZarateSP1._2
             ucEspecialidades1.LoadEspecialidades();
             ucMedicos1.CargarEspecialidades();
         }
+
+ 
     }
 }
